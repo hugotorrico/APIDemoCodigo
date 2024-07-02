195660ab51904dc24e82b089df4bb52daeba529b
Hugo Torrico <torricomarquezh@gmail.com>
2024-07-01 20:29:33 -0500

    Agregar archivos de proyecto.

---
+++ APIDemoCodigo/Controllers/PersonasController.cs
@@ -0,0 +1,52 @@
+﻿using APIDemoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APIDemoCodigo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        public static List<Persona> personas = new List<Persona>();
+
+        [HttpGet]
+        public List<Persona> Listar()
+        {                
+            return personas;
+        }
+
+        [HttpGet]
+        public Persona BuscarPorId(int Id)
+        {
+            Persona persona = personas.Where(x => x.Id == Id).FirstOrDefault();
+           
+            return persona;
+        }
+
+        [HttpPost]
+        public void Insertar( [FromBody] Persona persona)
+        {
+            personas.Add(persona);
+        }
+
+        [HttpPut]
+        public void Actualizar([FromBody] Persona persona, int Id )
+        {
+            Persona personaActualizar = personas.Where(x => x.Id == Id).FirstOrDefault();
+            personaActualizar.Nombres = persona.Nombres;
+            personaActualizar.Apellidos = persona.Apellidos;
+
+        }
+
+        [HttpDelete]
+        public void Eliminar(int Id)
+        {
+            Persona personaEliminar = personas.Where(x => x.Id == Id).FirstOrDefault();
+            personas.Remove(personaEliminar);
+
+        }
+
+    }
+}
---
+++ APIDemoCodigo/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace APIDemoCodigo.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+    }
+}
